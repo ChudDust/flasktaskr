cff3215e3d9b6c44dc464b54571d99c4a9a89ba2
Chad Elder <chud92@msn.com>
2016-02-08 22:35:21 -0500

    SQLAlchemy

--- _config.py
+++ _config.py
@@ -14,3 +14,6 @@ SECRET_KEY = 'my_precious'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database url
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
